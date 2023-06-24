d0b1df9957938a2670aee85874f1edd7d9e928da
Benjamin Yong <benj_yongzh@hotmail.com>
2023-06-24 16:54:03 +0800

    Styling improvement for navbar. Dropdown has no animations yet.

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,6 +1,8 @@
 "use client";
+import { useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import SwitchToggle from "./SwitchToggle";
 
 const routes = [
   { href: "/", text: "Home" },
@@ -10,19 +12,56 @@ const routes = [
 ];
 
 const Nav = () => {
+  const [displayMode, setDisplayMode] = useState<string>("light");
+  const [showDropdown, setShowDropdown] = useState<boolean>(false);
   const pathname = usePathname();
+
+  const useLightMode = () => {
+    console.log("nav setting to light mode");
+    setDisplayMode("light");
+  };
+  const useDarkMode = () => {
+    console.log("nav setting to dark mode");
+    setDisplayMode("dark");
+  };
+
+  const toggleDropdown = () => {
+    setShowDropdown((curr) => !curr);
+  };
+
   return (
-    <nav className="absolute top-0 flex justify-end w-screen gap-5">
-      {routes.map((route) => (
-        <Link
-          className={`nav-link group ${
-            pathname === route.href ? "font-bold text-primarydarkmode" : null
-          }`}
-          href={route.href}
+    <nav className="absolute top-0 flex items-center justify-end w-screen gap-5 px-5 py-3 sm:gap-7">
+      <SwitchToggle
+        setLightMode={useLightMode}
+        setDarkMode={useDarkMode}
+        displayState={displayMode}
+      />
+      <div className="nav-dropdown">
+        <button
+          className="nav-dropdown-btn"
+          type="button"
+          onClick={() => toggleDropdown()}
         >
-          {route.text}
-        </Link>
-      ))}
+          drop
+        </button>
+        <ul
+          className={`nav-dropdown-menu ${showDropdown ? "visible" : "hidden"}`}
+        >
+          {routes.map((route) => (
+            <Link
+              className={`nav-link group ${
+                pathname === route.href
+                  ? "font-bold text-primarydarkmode"
+                  : null
+              }`}
+              href={route.href}
+              key={route.text}
+            >
+              {route.text}
+            </Link>
+          ))}
+        </ul>
+      </div>
     </nav>
   );
 };
