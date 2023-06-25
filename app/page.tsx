66c51195ed9d5154d5858ff9f3533795e872b240
Benjamin Yong <benj_yongzh@hotmail.com>
2023-06-25 18:27:10 +0800

    Use primary button for projects link in landing page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import ButtonNext from "@/components/ButtonNext";
 
 export default function Home() {
   return (
@@ -65,9 +64,22 @@ export default function Home() {
       </p>
 
       <div className="flex items-center self-end justify-center gap-4 mt-4">
-        <span className="text-2xl text-textlightmode">Projects</span>
-
-        <ButtonNext href="/projects" />
+        <Link href="/projects" className="btn-primary" type="button">
+          Projects
+          <svg
+            aria-hidden="true"
+            className="w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+        </Link>
       </div>
     </div>
   );
