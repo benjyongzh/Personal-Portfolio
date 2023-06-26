7107a1737980c6f5b66ae2e0c663dbb5bf087382
Benjamin Yong <benj_yongzh@hotmail.com>
2023-06-26 16:05:24 +0800

    Add component to do global screen greying out

---
+++ components/ScreenGreyOut.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+//redux
+// import { useSelector } from "react-redux";
+import { useAppSelector, useAppDispatch } from "@/hooks/reduxHooks";
+import { removePopup } from "@/features/popup/popupSlice";
+import { useEffect } from "react";
+
+import { motion } from "framer-motion";
+
+const screenGreyOutVariant = {
+  hidden: {
+    opacity: 0,
+    transition: {
+      type: "tween",
+      duration: 0.2,
+    },
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      type: "tween",
+      duration: 0.2,
+    },
+  },
+};
+
+const ScreenGreyOut = () => {
+  const dispatch = useAppDispatch();
+  const currentPopups = useAppSelector((state) => state.popup.popups);
+
+  const clicked = () => {
+    dispatch(removePopup(currentPopups[currentPopups.length - 1]));
+  };
+
+  useEffect(() => {
+    if (currentPopups.length) {
+      //make document body be unclickable
+      //   body.has-popup
+      if (!document.body.classList.contains("has-popup"))
+        document.body.classList.add("has-popup");
+    } else {
+      if (document.body.classList.contains("has-popup"))
+        document.body.classList.remove("has-popup");
+    }
+
+    return () => {
+      //remove document unclickable class
+      //   body.has-popup
+      document.body.classList.remove("has-popup");
+    };
+  }, [currentPopups]);
+
+  return (
+    <motion.div
+      variants={screenGreyOutVariant}
+      initial="hidden"
+      animate={currentPopups.length > 0 ? "visible" : "hidden"}
+      className={`${
+        currentPopups.length > 0 ? "z-10" : "pointer-events-none"
+      } fixed mx-auto left-0 right-0 flex my-auto top-0 bottom-0 items-center justify-center w-screen h-screen`}
+      style={{ backgroundColor: "#020617b4" }}
+      //   onClick={() => {
+      //     clicked();
+      //   }}
+    ></motion.div>
+  );
+};
+
+export default ScreenGreyOut;
