b0008e703650b9f203ba2d0a521dcb6b9cec128f
Benjamin Yong <benj_yongzh@hotmail.com>
2023-06-26 23:36:37 +0800

    Prepare for full page webapp.

---
+++ sections/ResumeSection.tsx
@@ -0,0 +1,9 @@
+export default function Resume() {
+  return (
+    <div className="flex flex-col items-start gap-6 justify-stretch sm:px-12 sm:py-10 app">
+      <header className="text-3xl font-light tracking-wide sm:text-5xl">
+        RESUME
+      </header>
+    </div>
+  );
+}
