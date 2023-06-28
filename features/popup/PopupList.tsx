cfc4f0f663648120ab6e75645edd26263ef6274b
Benjamin Yong <benj_yongzh@hotmail.com>
2023-06-28 22:18:13 +0800

    Use switch for choosing type of popup component

--- features/popup/PopupList.tsx
+++ features/popup/PopupList.tsx
@@ -1,21 +1,35 @@
 import { AnimatePresence } from "framer-motion";
+// import { useEffect, useState } from "react";
 
 import { useAppSelector } from "@/hooks/reduxHooks";
-import { IPopup, IPopupType } from "./popupSlice";
+import { IPopup } from "./popupSlice";
 // import projectList, { projectReference } from "@/lib/projectList";
 import ProjectPopup from "./ProjectPopup";
 
 const PopupList = () => {
   const currentPopups = useAppSelector((state) => state.popup.popups);
 
+  const getPopupElement = (popup: IPopup) => {
+    switch (popup.type!.type) {
+      case "projectPopup":
+        return (
+          <ProjectPopup
+            project={popup.type!.info}
+            popupId={popup.id}
+            key={popup.id}
+          />
+        );
+        break;
+
+      default:
+        break;
+    }
+  };
+
   return (
     <AnimatePresence>
       {currentPopups.length > 0
-        ? currentPopups!.map((popup: IPopup) =>
-            popup.type!.type === "project" ? (
-              <ProjectPopup project={popup.type!.info} popupId={popup.id} />
-            ) : null
-          )
+        ? currentPopups.map((popup: IPopup) => getPopupElement(popup))
         : null}
     </AnimatePresence>
   );
