938aedadf59114f2cd5e76a54155cf18da963c99
Benjamin Yong <benj_yongzh@hotmail.com>
2023-06-30 11:18:08 +0800

    Add lib for text for about page.

---
+++ lib/aboutText.tsx
@@ -0,0 +1,9 @@
+const aboutText = {
+  pageTitle: "ABOUT",
+  header: "This is about me",
+  linkedinLink: "/",
+  githubLink: "https://github.com/benjyongzh",
+  description: "lorem20",
+};
+
+export default aboutText;
