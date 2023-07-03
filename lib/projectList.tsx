348158a026732dec255c6abda2da60dbf0a5b26c
Benjamin Yong <benj_yongzh@hotmail.com>
2023-07-03 18:20:34 +0800

    Use array of src strings for images.

--- lib/projectList.tsx
+++ lib/projectList.tsx
@@ -7,7 +7,7 @@ export interface projectReference {
   cardDescription: string;
   longDescription?: string;
   cardImage?: string;
-  popupImage?: string;
+  detailImages?: Array<string>;
   techStack: tech.techStack;
 }
 
@@ -62,6 +62,14 @@ const projectList: projectReference[] = [
     href: "https://inventory-application-production-a970.up.railway.app/",
     cardDescription: "A simple app for registering the statuses of inventory.",
     longDescription: "another long description here",
+    cardImage: "/assets/images/inventory-application-card-image.JPG",
+    detailImages: [
+      "/assets/images/inventory-application-image-1.JPG",
+      "/assets/images/inventory-application-image-2.JPG",
+      "/assets/images/inventory-application-image-3.JPG",
+      "/assets/images/inventory-application-image-4.JPG",
+      "/assets/images/inventory-application-image-5.JPG",
+    ],
     techStack: [tech.bootstrap, tech.express, tech.mongodb, tech.nodejs],
   },
   {
@@ -70,6 +78,12 @@ const projectList: projectReference[] = [
     href: "https://benjyongzh.github.io/Weather-App/",
     cardDescription: "Simple API fetching to publicly-available live data",
     longDescription: "guess whats the weather today lmao",
+    cardImage: "/assets/images/weather-app-card-image.JPG",
+    detailImages: [
+      "/assets/images/weather-app-image-1.JPG",
+      "/assets/images/weather-app-image-2.JPG",
+      "/assets/images/weather-app-image-3.JPG",
+    ],
     techStack: [tech.html, tech.css, tech.javascript],
   },
   {
@@ -78,6 +92,12 @@ const projectList: projectReference[] = [
     href: "https://benjyongzh.github.io/Memory-Card/",
     cardDescription: "A game of remembering what you last clicked",
     longDescription: "for animal lovers for sure",
+    cardImage: "/assets/images/memory-card-card-image.JPG",
+    detailImages: [
+      "/assets/images/memory-card-image-1.JPG",
+      "/assets/images/memory-card-image-2.JPG",
+      "/assets/images/memory-card-image-3.JPG",
+    ],
     techStack: [tech.react, tech.css],
   },
   {
@@ -87,6 +107,14 @@ const projectList: projectReference[] = [
     cardDescription: "Finding the shortest path for a Knight to reach his goal",
     longDescription:
       "Find out how many steps it takes for a knight piece to reach a particular board position",
+    cardImage: "/assets/images/knights-travails-card-image.JPG",
+    detailImages: [
+      "/assets/images/knights-travails-image-1.JPG",
+      "/assets/images/knights-travails-image-2.JPG",
+      "/assets/images/knights-travails-image-3.JPG",
+      "/assets/images/knights-travails-image-4.JPG",
+      "/assets/images/knights-travails-image-5.JPG",
+    ],
     techStack: [tech.html, tech.css, tech.javascript],
   },
   {
