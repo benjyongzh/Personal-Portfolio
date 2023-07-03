7568602140ea38675a927f61dd60dd990e84e6ab
Benjamin Yong <benj_yongzh@hotmail.com>
2023-07-03 17:22:09 +0800

    Add styling for project cards.

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -7,6 +7,8 @@ import ScrollAnimationWrapper, {
 import { useAppSelector } from "@/hooks/reduxHooks";
 import { getRandomArbitrary } from "@/utils/numbers";
 import { projectCardVariant } from "@/lib/framerVariants";
+import { projectReference } from "@/lib/projectList";
+import TechIcon from "./TechIcon";
 
 const animationPropsXS: IScrollAnimations = {
   yIn: 100,
@@ -15,18 +17,18 @@ const animationPropsXS: IScrollAnimations = {
 };
 
 const ProjectCard = (props: {
-  projectName?: string;
+  project: projectReference;
   click: Function;
   index: number;
 }) => {
   const currentBreakpoint: string = useAppSelector(
     (state) => state.display.currentBreakpoint
   );
+  const { project } = props;
 
   const getAnimPropsFromIndex = (index: number) => {
     const randomStartDistance = getRandomArbitrary(0, 150);
-    const randomEndDistance =
-      randomStartDistance + getRandomArbitrary(-150, 150);
+    const randomEndDistance = randomStartDistance + getRandomArbitrary(-75, 75);
     const randomXDistance = getRandomArbitrary(-30, 30);
     const newProps: IScrollAnimations = {
       xIn: randomXDistance,
@@ -40,9 +42,27 @@ const ProjectCard = (props: {
     return newProps;
   };
 
+  const cardDetails = (
+    <div className="flex flex-col items-start justify-between gap-3">
+      <header
+        className={`text-accentlightmode dark:text-accentdarkmode font-bold text-lg`}
+      >
+        {project.projectName}
+      </header>
+      {currentBreakpoint !== "xs" ? <div className="w-full">image</div> : null}
+
+      <p className="pageText text-start">{project.cardDescription}</p>
+      <div className="flex flex-wrap items-center justify-center w-full gap-2 pt-4 border-t-2 justify-self-end border-secondarydarkmode dark:border-accentdarkmode">
+        {project.techStack.map((tech) => (
+          <TechIcon tech={tech} size="xs" />
+        ))}
+      </div>
+    </div>
+  );
+
   return currentBreakpoint === "xs" ? (
     <motion.button
-      className="flex flex-col items-start justify-between bg-opacity-80 backdrop-blur bg-secondarylightmode/50 dark:bg-secondarylightmode/10 rounded-[24px] max-w-lg h-52 sm:h-80 min-w-[200px] gap-3 p-5 shadow-xl hover:z-[2] hover:shadow-2xl hover:shadow-slate-500 dark:hover:shadow-slate-900"
+      className="flex flex-col items-stretch justify-between bg-opacity-80 backdrop-blur bg-secondarylightmode/50 dark:bg-secondarylightmode/10 rounded-[24px] max-w-lg min-w-[200px] gap-3 p-5 shadow-xl hover:z-[2] hover:shadow-2xl hover:shadow-slate-500 dark:hover:shadow-slate-900"
       type="button"
       initial="hidden"
       whileInView="visible"
@@ -53,16 +73,12 @@ const ProjectCard = (props: {
         props.click();
       }}
     >
-      <header
-        className={`text-accentlightmode dark:text-accentdarkmode font-bold text-lg`}
-      >
-        {props.projectName}
-      </header>
+      {cardDetails}
     </motion.button>
   ) : (
     <ScrollAnimationWrapper animationProps={getAnimPropsFromIndex(props.index)}>
       <motion.button
-        className="flex flex-col items-start justify-between bg-opacity-80 backdrop-blur bg-secondarylightmode/50 dark:bg-secondarylightmode/10 rounded-[24px] max-w-lg h-52 sm:h-80 min-w-[200px] gap-3 p-5 shadow-xl hover:z-[2] hover:shadow-2xl hover:shadow-slate-500 dark:hover:shadow-slate-900"
+        className="flex flex-col items-stretch justify-between bg-opacity-80 backdrop-blur bg-secondarylightmode/50 dark:bg-secondarylightmode/10 rounded-[24px] max-w-lg min-w-[200px] gap-3 p-5 shadow-xl hover:z-[2] hover:shadow-2xl hover:shadow-slate-500 dark:hover:shadow-slate-900"
         type="button"
         transition={{ type: "spring", duration: 0.4 }}
         layout
@@ -71,11 +87,7 @@ const ProjectCard = (props: {
           props.click();
         }}
       >
-        <header
-          className={`text-accentlightmode dark:text-accentdarkmode font-bold text-lg`}
-        >
-          {props.projectName}
-        </header>
+        {cardDetails}
       </motion.button>
     </ScrollAnimationWrapper>
   );
