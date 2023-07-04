8a0288c301df8199320943da5c9c3656e90114c4
Benjamin Yong <benj_yongzh@hotmail.com>
2023-07-04 17:16:08 +0800

    Add navigation dots and correct image positioning.

--- components/ImageCarousel.tsx
+++ components/ImageCarousel.tsx
@@ -3,9 +3,13 @@
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { useState } from "react";
+import { useAppSelector } from "@/hooks/reduxHooks";
 
 const ImageCarousel = (props: { images: Array<string> }) => {
   const [currentPosition, setCurrentPosition] = useState(0);
+  const currentBreakpoint: string = useAppSelector(
+    (state) => state.display.currentBreakpoint
+  );
   const { images } = props;
 
   const shiftImages = (position: number) => {
@@ -14,35 +18,99 @@ const ImageCarousel = (props: { images: Array<string> }) => {
     setCurrentPosition((curr) => curr + position);
   };
 
+  const setImageIndex = (position: number) => {
+    setCurrentPosition(position);
+  };
+
   return (
-    <div className="relative flex items-stretch justify-center w-full h-48">
-      <button
-        className="h-full p-3 opacity-70 justify-self-start bg-secondarydarkmode-light dark:bg-secondarylightmode-dark hover:bg-secondarydarkmode dark:hover:bg-secondarylightmode"
-        type="button"
-        onClick={() => shiftImages(-1)}
-      >
-        L
-      </button>
-      {images?.map((image, i) => (
-        <motion.div
-          className="h-full px-4 -z-10"
-          animate={{
-            x: -currentPosition * 100,
-            scale: currentPosition === i ? 1.2 : 1,
-            opacity: currentPosition === i ? 1 : 0.8,
-          }}
-          transition={{ duration: 0.2 }}
-        >
-          <Image src={image} width={500} height={500} alt={image} />
-        </motion.div>
-      ))}
-      <button
-        className="h-full p-3 opacity-70 justify-self-start bg-secondarydarkmode-light dark:bg-secondarylightmode-dark hover:bg-secondarydarkmode dark:hover:bg-secondarylightmode"
-        type="button"
-        onClick={() => shiftImages(1)}
-      >
-        R
-      </button>
+    <div className="flex flex-col items-center justify-between w-full gap-5">
+      <div className="flex items-center justify-between w-full overflow-hidden h-60">
+        {currentBreakpoint === "xs" ? null : (
+          <button
+            className="p-3 rounded-full opacity-70 justify-self-start bg-secondarydarkmode-light dark:bg-secondarylightmode-dark hover:bg-secondarydarkmode dark:hover:bg-secondarylightmode aspect-square"
+            type="button"
+            onClick={() => shiftImages(-1)}
+          >
+            <svg
+              aria-hidden="true"
+              className="w-5 h-5 rotate-180"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </button>
+        )}
+
+        <div className="relative flex items-center w-full h-full">
+          {images?.map((image, i) => (
+            <motion.div
+              className={`absolute left-[50%] overflow-hidden h-full w-[240px] sm:w-[320px] rounded-xl -z-10 ${
+                currentPosition === i ? "shadow-xl" : "shadow-md"
+              }`}
+              animate={{
+                x:
+                  currentBreakpoint === "xs"
+                    ? (i - currentPosition) * 240 - 120
+                    : (i - currentPosition) * 320 - 160,
+                scale: currentPosition === i ? 1 : 0.8,
+                opacity: currentPosition === i ? 1 : 0.8,
+              }}
+              transition={{ type: "tween", duration: 0.15 }}
+            >
+              <Image
+                src={image}
+                fill={true}
+                alt={image}
+                style={{ objectFit: "cover" }}
+              />
+            </motion.div>
+          ))}
+        </div>
+
+        {currentBreakpoint === "xs" ? null : (
+          <button
+            className="p-3 rounded-full opacity-70 justify-self-end bg-secondarydarkmode-light dark:bg-secondarylightmode-dark hover:bg-secondarydarkmode dark:hover:bg-secondarylightmode aspect-square"
+            type="button"
+            onClick={() => shiftImages(1)}
+          >
+            <svg
+              aria-hidden="true"
+              className="w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </button>
+        )}
+      </div>
+      <div className="flex items-center justify-center w-full h-3 gap-3">
+        {images?.map((image, i) => (
+          <motion.button
+            className={`h-full rounded-full cursor-pointer aspect-square bg-primarylightmode hover:bg-primarylightmode-light ${
+              currentPosition === i
+                ? "bg-primarylightmode"
+                : "bg-primarydarkmode hover:bg-primarydarkmode-light brightness-90"
+            }`}
+            animate={{
+              scale: currentPosition === i ? 1 : 0.75,
+            }}
+            transition={{ type: "tween", duration: 0.15 }}
+            onClick={() => setImageIndex(i)}
+          ></motion.button>
+        ))}
+      </div>
     </div>
   );
 };
