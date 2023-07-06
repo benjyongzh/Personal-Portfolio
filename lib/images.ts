a09464c32f8db608a1b75be2dccc00d5b63f9a13
Benjamin Yong <benj_yongzh@hotmail.com>
2023-07-06 10:04:51 +0800

    Create main interface for image popup

--- lib/images.ts
+++ lib/images.ts
@@ -1,6 +1,24 @@
+import { isArrayOfInterface } from "@/utils/objects";
+
 export interface imageReference {
   image: string;
   name: string;
+}
+
+export interface slideshowInfo {
+  currentImage: imageReference;
+  images: imageReference[];
+}
+
+export function isSlideshowInfo(arg: any): arg is slideshowInfo {
+  return (
+    arg &&
+    arg.currentImage &&
+    isimageReference(arg.currentImage) &&
+    arg.images &&
+    Array.isArray(arg.images) &&
+    isArrayOfInterface(arg.images, isimageReference)
+  );
 }
 
 export function isimageReference(arg: any): arg is imageReference {
