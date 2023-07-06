d296319f6c845dc21599e6586f8ca27380f58a82
Benjamin Yong <benj_yongzh@hotmail.com>
2023-07-06 23:35:13 +0800

    Add email and phone number info and icon.

--- lib/urls.tsx
+++ lib/urls.tsx
@@ -2,3 +2,6 @@ export const linkedinProfileURL =
   "https://www.linkedin.com/in/benjamin-yong-054a48123/";
 export const githubProfileURL = "https://github.com/benjyongzh";
 export const githubAPIRepoBaseURL = "https://api.github.com/repos/benjyongzh";
+export const phoneNumber = "+6596742741";
+export const phoneNumberText = "+65 9674 2741";
+export const email = "benj_yongzh@hotmail.com";
