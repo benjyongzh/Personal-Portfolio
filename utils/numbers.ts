365b7adbf77075eb305f5e64fb04e7c3dde4200e
Benjamin Yong <benj_yongzh@hotmail.com>
2023-07-06 10:03:50 +0800

    Create function for making number arrays.

--- utils/numbers.ts
+++ utils/numbers.ts
@@ -17,3 +17,11 @@ function getRandomInt(min: number, max: number) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+export const createNumberArray = (count: number, startingNumber = 0) => {
+  let newArray: Array<number> = [];
+  for (let i = startingNumber; i < count; i++) {
+    newArray.push(i);
+  }
+  return newArray;
+};
