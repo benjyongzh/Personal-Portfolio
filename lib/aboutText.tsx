85e6e59aed626649a04bf106f463f2e3bcda2019
Benjamin Yong <benj_yongzh@hotmail.com>
2023-07-06 22:10:10 +0800

    Add description.

--- lib/aboutText.tsx
+++ lib/aboutText.tsx
@@ -1,9 +1,15 @@
 const aboutText = {
   pageTitle: "ABOUT",
-  header: "This is about me",
+  header: "Nice to meet you!",
   linkedinLink: "/",
   githubLink: "https://github.com/benjyongzh",
-  description: "lorem20",
+  description: [
+    "Hi there! I am Benjamin, and I am a self-taught software developer in Singapore. My journey started when I was 15 and tinkered with game development as a hobby, albeit only within the confines of Warcraft3 custom map creations. It was then that I grew to enjoy problem-solving towards my own set of goals whilst creating systems using programming logic. I was fascinated at how a game could run exactly the way I wanted it to. I grew the habit of finding resources online to see how others have tackled the same problems I encountered. Back then, StackOverflow just started. Alas, game development remained as a dormant hobby for the next decade, but it always lingered at the back of my mind.",
+    "The Covid lockdown in 2020 forced me to be indoors much more, and it sparked an old flame to revisit game creation, this time with a friend, and within DotA2 custom games. There was a nice global community of such developers, each striving to push the limits of the game engine, and I was part of it. However, eventually my friend and I decided that our game idea was not feasible within this game engine, and we gradually ceased development.",
+    "A year later, I decided to try my hand at development again after I read up on Unity. I spent the next 4 months developing the game in Unity, from scratch. It eventually led to nothing, admittedly because the game idea was way too ambitious for our skills, time and manpower. Development waned, but I am proud of how much I learned within that time period.",
+    "In 2022, I decided that, given my drive in solving problems in development and programming, I ought to see how far I could go with them. I started reading up about software engineering, what it entails, and I embarked on The Odin Project. It gave me more material to study software development, best practices, web technologies, and projects to practise my knowledge. I have been putting in consistent effort almost everyday to pick up skills in front and back-end development, even after work hours. While I have finished The Odin Project, I intend to continue pursuing software engineering, both as a career and through my pet project ideas.",
+    "I hope to work with you soon!",
+  ],
 };
 
 export default aboutText;
