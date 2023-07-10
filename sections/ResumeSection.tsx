16b149b29d799dc393b1c6ab2a64c9e313371fcf
Benjamin Yong <benj_yongzh@hotmail.com>
2023-07-10 10:55:58 +0800

    Create skills function. SHould create a separate component

--- sections/ResumeSection.tsx
+++ sections/ResumeSection.tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 import resumeText from "@/lib/resumeText";
 import Tooltip from "@/components/Tooltip";
 import TechIcon from "@/components/TechIcon";
+import { tech } from "@/lib/techStackList";
 
 //redux
 // import { useDispatch, useSelector } from "react-redux";
@@ -28,16 +29,37 @@ export default function Resume() {
     (state) => state.display.currentBreakpoint
   );
 
-  const techs = resumeText.skills.map((tech, i) => (
-    <motion.p key={i} className="pageText pageText-bodytext">
-      <TechIcon tech={tech} size="md" key={i} popup={false} />
-    </motion.p>
-  ));
+  const skills = (skillset: object) => {
+    const domains: string[] = Object.keys(skillset);
+    return domains.map((domain) => {
+      const domainSkills: Array<tech> =
+        skillset[domain as keyof typeof skillset];
+      const icons = techlist(domainSkills);
+      return (
+        <div className="flex flex-col items-center justify-start gap-3 p-4 sm:gap-5 pageText">
+          <div className="w-full text-lg text-center pageText sm:text-xl">
+            {domain}
+          </div>
+          <ul className="flex flex-wrap items-start justify-center w-full gap-3">
+            {icons}
+          </ul>
+        </div>
+      );
+    });
+  };
+
+  const techlist = (domain: Array<tech>) => {
+    return domain.map((tech, i) => (
+      <motion.p key={i} className="pageText pageText-bodytext">
+        <TechIcon tech={tech} size="md" key={i} popup={true} />
+      </motion.p>
+    ));
+  };
 
   const professions = resumeText.professions.map((job, i) => (
     <motion.div
       key={i}
-      className="flex flex-col items-stretch justify-start gap-3"
+      className="flex flex-col items-stretch justify-start gap-4 pageText"
     >
       <div className="flex items-center justify-between">
         <div>
@@ -47,9 +69,9 @@ export default function Resume() {
           {job.yearStarted} - {job.yearEnded}
         </div>
       </div>
-      <ul className="flex flex-col items-start justify-start">
+      <ul className="flex flex-col items-start justify-start gap-2">
         {job.description.map((item) => (
-          <li className="text-justify list-none">- {item}</li>
+          <li className="list-none">- {item}</li>
         ))}
       </ul>
     </motion.div>
@@ -58,7 +80,7 @@ export default function Resume() {
   const educations = resumeText.educations.map((degree, i) => (
     <motion.div
       key={i}
-      className="flex flex-col items-stretch justify-start gap-3"
+      className="flex flex-col items-stretch justify-start gap-4 pageText"
     >
       <div className="flex items-center justify-between">
         <div>
@@ -68,9 +90,9 @@ export default function Resume() {
           {degree.yearStarted} - {degree.yearEnded}
         </div>
       </div>
-      <ul className="flex flex-col items-start justify-start">
+      <ul className="flex flex-col items-start justify-start gap-2">
         {degree.description.map((item) => (
-          <li className="text-justify list-none">- {item}</li>
+          <li className="list-none">- {item}</li>
         ))}
       </ul>
     </motion.div>
@@ -79,7 +101,7 @@ export default function Resume() {
   return (
     <motion.section
       id="resume-section"
-      className="flex flex-col items-start justify-start gap-12 overflow-x-hidden sm:gap-16 section"
+      className="flex flex-col items-stretch justify-start gap-12 py-20 overflow-x-hidden sm:gap-16 section"
     >
       {/* <ScrollAnimationWrapper
         animationProps={{
@@ -96,30 +118,30 @@ export default function Resume() {
       {/* SKILLS */}
       {/* </ScrollAnimationWrapper> */}
       <div className="flex flex-col items-stretch justify-start gap-4">
-        <div className="flex items-center justify-center pageText pageText-header">
-          Skills
+        <div className="flex items-center justify-center py-2 border-t-2 border-b-2 pageText pageText-header border-secondarydarkmode dark:border-accentdarkmode">
+          SKILLS
         </div>
-        <div className="flex flex-wrap items-center justify-center w-full gap-2 border-t-2 border-secondarydarkmode dark:border-accentdarkmode">
-          {techs}
+        <div className="grid w-full grid-flow-row grid-cols-1 gap-6 sm:gap-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {skills(resumeText.skills)}
         </div>
       </div>
 
       {/* PROFESSION */}
       <div className="flex flex-col items-stretch justify-start gap-4">
-        <div className="flex items-center justify-center pageText pageText-header">
-          Professional Experience
+        <div className="flex items-center justify-center py-2 border-t-2 border-b-2 pageText pageText-header border-secondarydarkmode dark:border-accentdarkmode">
+          PROFESSIONAL EXPERIENCE
         </div>
-        <div className="flex flex-col items-stretch justify-start w-full gap-2 border-t-2 border-secondarydarkmode dark:border-accentdarkmode">
+        <div className="flex flex-col items-stretch justify-start w-full gap-6">
           {professions}
         </div>
       </div>
 
       {/* EDUCATION */}
       <div className="flex flex-col items-stretch justify-start gap-4">
-        <div className="flex items-center justify-center pageText pageText-header">
-          Education
+        <div className="flex items-center justify-center py-2 border-t-2 border-b-2 pageText pageText-header border-secondarydarkmode dark:border-accentdarkmode">
+          EDUCATION
         </div>
-        <div className="flex flex-col items-stretch justify-start w-full gap-2 border-t-2 border-secondarydarkmode dark:border-accentdarkmode">
+        <div className="flex flex-col items-stretch justify-start w-full gap-6">
           {educations}
         </div>
       </div>
